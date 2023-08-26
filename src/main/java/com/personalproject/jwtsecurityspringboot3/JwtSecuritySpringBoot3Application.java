54debe96999b47ff29ba514cc00e7344889acd62
Mateja Musa <matejamusa6@gmail.com>
2023-08-26 19:34:32 +0200

    initial commit

---
+++ src/main/java/com/personalproject/jwtsecurityspringboot3/JwtSecuritySpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.personalproject.jwtsecurityspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecuritySpringBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecuritySpringBoot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/personalproject/jwtsecurityspringboot3/JwtSecuritySpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproject.jwtsecurityspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecuritySpringBoot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
