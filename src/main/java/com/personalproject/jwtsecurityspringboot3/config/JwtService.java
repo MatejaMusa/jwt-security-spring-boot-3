bb9ed29d87bd5e22717d91956118b812f2dd66fb
Mateja Musa <matejamusa6@gmail.com>
2023-08-27 14:42:22 +0200

    add dependency to pom.xml and add jwt authentication filter

---
+++ src/main/java/com/personalproject/jwtsecurityspringboot3/config/JwtService.java
@@ -0,0 +1,38 @@
+package com.personalproject.jwtsecurityspringboot3.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY = "b7bb4eaca696bce1218188fdb8bb1a3f275279dbe1de0d63c1ba60fa8e0cd3bc";
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
